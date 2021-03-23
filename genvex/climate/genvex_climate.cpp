7391d0b9ac09c73a55b6217c3367306936c60c02
Samuel Sieb <samuel@sieb.net>
2021-03-23 01:12:13 -0700

    track target temp and fan speed

--- genvex/climate/genvex_climate.cpp
+++ genvex/climate/genvex_climate.cpp
@@ -11,6 +11,21 @@ void GenvexClimate::setup() {
     current_temperature = state;
     publish_state();
   });
+  genvex_->add_target_temp_callback([this](float state) {
+    target_temperature = state;
+    publish_state();
+  });
+  genvex_->add_fan_speed_callback([this](int state) {
+    climate::ClimateFanMode fmode;
+    switch (state) {
+      case 2: fmode = climate::CLIMATE_FAN_LOW; break;
+      case 3: fmode = climate::CLIMATE_FAN_MEDIUM; break;
+      case 4: fmode = climate::CLIMATE_FAN_HIGH; break;
+      default: fmode = climate::CLIMATE_FAN_OFF; break;
+    }
+    fan_mode = fmode;
+    publish_state();
+  });
   current_temperature = sensor_->state;
   target_temperature = sensor_->state;
   mode = climate::CLIMATE_MODE_HEAT;
@@ -18,15 +33,14 @@ void GenvexClimate::setup() {
 }
 
 void GenvexClimate::control(const climate::ClimateCall &call) {
-  if (call.get_target_temperature_low().has_value())
-    ;
+  //if (call.get_target_temperature_low().has_value())
+  //  ;
   //this->publish_state();
 }
 
 climate::ClimateTraits GenvexClimate::traits() {
   auto traits = climate::ClimateTraits();
   traits.set_supports_current_temperature(true);
-  traits.set_supports_fan_mode_on(true);
   traits.set_supports_fan_mode_off(true);
   traits.set_supports_fan_mode_low(true);
   traits.set_supports_fan_mode_medium(true);
