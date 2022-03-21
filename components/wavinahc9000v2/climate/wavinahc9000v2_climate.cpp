1e43e1bee2ad9d18338e099ab898640a316b6150
heinekmadsen <48906528+heinekmadsen@users.noreply.github.com>
2022-03-21 22:00:34 +0100

    Added thermostat action
    To show if the channel is currently heating or off

--- components/wavinahc9000v2/climate/wavinahc9000v2_climate.cpp
+++ components/wavinahc9000v2/climate/wavinahc9000v2_climate.cpp
@@ -27,6 +27,16 @@ void Wavinahc9000v2Climate::setup() {
     }
     publish_state();
   });
+  hvac_action_->add_on_state_callback([this](bool state) {
+    ESP_LOGD(TAG, "Current action is : %s", ONOFF(state));
+    if (state) {
+      this->action = climate::CLIMATE_ACTION_HEATING;
+    }
+    else if (!state) {
+      this->action = climate::CLIMATE_ACTION_OFF;
+    }
+    publish_state();
+  });
 
   current_temperature = current_temp_sensor_->state;
   target_temperature  = temp_setpoint_number_->state;
@@ -69,6 +79,8 @@ climate::ClimateTraits Wavinahc9000v2Climate::traits() {
     climate::ClimateMode::CLIMATE_MODE_OFF,
     climate::ClimateMode::CLIMATE_MODE_AUTO,
   });
+
+  traits.set_supports_action(true);
 
   traits.set_supports_current_temperature(true);
   traits.set_visual_temperature_step(0.1);
