182131388594437664b4cee39b5fbb7b127b999f
Mogens <mogens@gnh.dk>
2022-07-16 21:00:46 +0200

    ..

--- components/sentio/climate/sentio_climate.cpp
+++ components/sentio/climate/sentio_climate.cpp
@@ -35,7 +35,8 @@ void SentioClimate::control(const climate::ClimateCall& call) {
     this->target_temperature = *call.get_target_temperature();
     float target = target_temperature;
     ESP_LOGD(TAG, "Target temperature changed to: %f", target);
-    temp_setpoint_number_->make_call().set_value(target).perform();//set(target);
+    temp_setpoint_number_->set(target);
+    //temp_setpoint_number_->make_call().set_value(target).perform();//set(target);
   }
 }
 
