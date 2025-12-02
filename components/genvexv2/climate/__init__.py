8d8dada1af3b70f7efc9719d6519bcf026547e21
heinekmadsen <48906528+heinekmadsen@users.noreply.github.com>
2025-12-02 14:37:41 +0100

    genvexv2.climate: migrate to climate.climate_schema() to fix CLIMATE_SCHEMA AttributeError on ESPHome 2025.11.x

--- components/genvexv2/climate/__init__.py
+++ components/genvexv2/climate/__init__.py
@@ -13,14 +13,14 @@ CONF_FAN_SPEED = "fan_speed_sensor_id"
 
 genvexv2_ns = cg.esphome_ns.namespace('genvexv2')
 Genvexv2Climate = genvexv2_ns.class_('Genvexv2Climate', climate.Climate, cg.Component)
- 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(Genvexv2Climate),
+
+# Migrate to new schema helper for ESPHome 2025.11+
+CONFIG_SCHEMA = climate.climate_schema(Genvexv2Climate).extend({
     cv.GenerateID(CONF_GENVEXV2_ID): cv.use_id(Genvexv2),
     cv.Required(CONF_TARGET_TEMP): cv.use_id(number.Number),
     cv.Required(CONF_CURRENT_TEMP): cv.use_id(sensor.Sensor),
     cv.Required(CONF_FAN_SPEED): cv.use_id(number.Number),
-    #cv.Required(CONF_MODE): cv.use_id(select.Select)
+    # cv.Required(CONF_MODE): cv.use_id(select.Select)
 }).extend(cv.COMPONENT_SCHEMA)
  
 def to_code(config):
