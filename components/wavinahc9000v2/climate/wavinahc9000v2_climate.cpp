1a4a08e71133c3e111a6b87926dfe80066e9aa73
78075 <mnicolaisen@grundfos.cpom>
2026-03-13 16:58:57 +0100

    Fix deprecated ClimateTraits: use add_feature_flags() instead of set_supports_*()

--- components/wavinahc9000v2/climate/wavinahc9000v2_climate.cpp
+++ components/wavinahc9000v2/climate/wavinahc9000v2_climate.cpp
@@ -79,9 +79,7 @@ climate::ClimateTraits Wavinahc9000v2Climate::traits() {
     climate::ClimateMode::CLIMATE_MODE_HEAT,
   });
 
-  traits.set_supports_action(true);
-
-  traits.set_supports_current_temperature(true);
+  traits.add_feature_flags(climate::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE | climate::CLIMATE_SUPPORTS_ACTION);
   traits.set_visual_temperature_step(0.5);
   traits.set_visual_min_temperature(6);
   traits.set_visual_max_temperature(40);
